ac7e230e230893c0127b3b2b31fc637c1f7a3c40
Lawrence Lockhart <ldlockhart@gmail.com>
2019-04-02 23:11:45 -0500

    add data file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -9,8 +11,8 @@ def index():
 def about():
     return render_template('about.html')
 @app.route('/articles')
-def about():
-    return render_template('articles.html')
+def articles():
+    return render_template('articles.html', articles=Articles)
 
 if (__name__) == ("__main__"):
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'author':'Lawrence Lockhart',
+            'create date':"April 2, 2019",
+            'body':'This is my first blog article. I should write about Flask because thats what Im learning and its amazing!'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'author':'Lawrence Lockhart',
+            'create date':"April 2, 2019",
+            'body':'This is my first blog article. I should write about Flask because thats what Im learning and its amazing!'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'author':'Lawrence Lockhart',
+            'create date':"April 2, 2019",
+            'body':'This is my first blog article. I should write about Flask because thats what Im learning and its amazing!'
+        }
+    ]
+    return articles
